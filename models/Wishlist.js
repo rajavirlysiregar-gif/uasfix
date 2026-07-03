7f2aec845fc346cfdacfc0cd5c934adbc42e7fdc
Raja Virly <rajavirlysiregar@gmail.com>
2026-07-03 22:59:10 +0700

    Fix database connection for Railway

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -1,13 +1,23 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db');
 
-const Wishlist = sequelize.define('Wishlist', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  buyerId: { type: DataTypes.INTEGER, allowNull: false },
-  productId: { type: DataTypes.INTEGER, allowNull: false }
-}, {
-  tableName: 'Wishlists',
-  indexes: [{ unique: true, fields: ['buyerId', 'productId'] }]
-});
+module.exports = (sequelize) => {
+  const Wishlist = sequelize.define('Wishlist', {
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true 
+    },
+    buyerId: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false 
+    },
+    productId: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false 
+    }
+  }, {
+    tableName: 'Wishlists'
+  });
 
-module.exports = Wishlist;
+  return Wishlist;
+};
