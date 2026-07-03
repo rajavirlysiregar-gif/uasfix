ffeaace4d6e3ed455bd108b0506729d11cf47220
Raja Virly <rajavirlysiregar@gmail.com>
2026-07-03 22:52:11 +0700

    Fix database connection for Railway

--- models/OrderItem.js
+++ models/OrderItem.js
@@ -1,16 +1,38 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db');
 
-const OrderItem = sequelize.define('OrderItem', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  orderId: { type: DataTypes.INTEGER, allowNull: false },
-  productId: { type: DataTypes.INTEGER, allowNull: false },
-  sellerId: { type: DataTypes.INTEGER, allowNull: false },
-  name: { type: DataTypes.STRING, allowNull: false }, // snapshot nama produk saat dibeli
-  price: { type: DataTypes.DECIMAL(12, 2), allowNull: false }, // snapshot harga saat dibeli
-  quantity: { type: DataTypes.INTEGER, allowNull: false }
-}, {
-  tableName: 'OrderItems'
-});
+module.exports = (sequelize) => {
+  const OrderItem = sequelize.define('OrderItem', {
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true 
+    },
+    orderId: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false 
+    },
+    productId: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false 
+    },
+    sellerId: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false 
+    },
+    quantity: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      defaultValue: 1,
+      validate: { min: 1 } 
+    },
+    price: { 
+      type: DataTypes.DECIMAL(12, 2), 
+      allowNull: false, 
+      validate: { min: 0 } 
+    }
+  }, {
+    tableName: 'OrderItems'
+  });
 
-module.exports = OrderItem;
+  return OrderItem;
+};
