c9b8679ac063b527aeb668d305f3f738598cc23e
Raja Virly <rajavirlysiregar@gmail.com>
2026-07-03 22:31:23 +0700

    Fix database connection for Railway

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
-const mysql = require("mysql2");
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  }
+);
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+module.exports = sequelize; // <--- PASTIKAN BARIS INI ADA
